f112da1efb4e21d78c82376b862a81e3f8b83d19
salvo1257 <giovane.pio@gmail.com>
2025-04-08 08:33:36 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('current-year').textContent = new Date().getFullYear();
-    console.log('CryptoRadar caricato!');
-});
