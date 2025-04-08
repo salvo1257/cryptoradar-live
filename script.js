126bbfa1d10156cb8d2ecc7607d2dcccfefea01a
salvo1257 <giovane.pio@gmail.com>
2025-04-08 08:31:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('current-year').textContent = new Date().getFullYear();
+    console.log('CryptoRadar caricato!');
+});
